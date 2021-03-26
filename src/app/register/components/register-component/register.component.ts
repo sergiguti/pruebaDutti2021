b1d27bd965d77d503419b8954cb74b4134dfc2d4
sergiguti <sergi.gutierrez04@gmail.com>
2021-03-26 21:55:29 +0100

    - Mantenemos las reglas de codificación que sugiere TSLint. - Pasamos el formatter que añado al repositorio para que todo el equipo mantenga el mismo formato de codigo. - Eliminación de codigo comentado y variables sin uso. - Cambios en el html manteniendo el idioma en los textos vistos por el usuario. - Creación de componente (error-messages) para mostrar los mensajes de error en los formularios.

--- src/app/register/components/register-component/register.component.ts
+++ src/app/register/components/register-component/register.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, Validators, FormBuilder } from '@angular/forms';
-import { Router } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
 
 // JSON
 import usersList from 'src/assets/json/users.json';
@@ -13,32 +13,30 @@ import usersList from 'src/assets/json/users.json';
 export class RegisterComponent implements OnInit {
 
   registerForm: FormGroup;
-  dataLoading: boolean = false;
+  dataLoading = false;
 
-  constructor(
-    private fb: FormBuilder,
-    private router: Router
-  ) { }
+  constructor(private fb: FormBuilder, private router: Router) {
+  }
 
   ngOnInit(): void {
     this.registerForm = this.fb.group({
-      first_name: [ '', [Validators.required, Validators.minLength(3)]],
-      last_name: [ '', [Validators.required, Validators.minLength(3)]],
-      username: [ '', [Validators.required, Validators.minLength(3)]],
-      email: [ '', [Validators.required, Validators.minLength(6)]],
+      first_name: ['', [Validators.required, Validators.minLength(3)]],
+      last_name: ['', [Validators.required, Validators.minLength(3)]],
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.minLength(6), Validators.email]],
 
-    })
+    });
   }
 
   registerUser() {
-    if (this.registerForm.invalid) { return }
-    // TODO : Falta integrar el servicio para registrar al usuario
-    // JSON simulando usuarios
-    var userLogin = this.registerForm.value;
-    usersList.push(userLogin)
-    console.log('User Register -->', usersList)
-    this.router.navigate(['/principal/ships'])
-
+    if (!this.registerForm.invalid) {
+      // TODO : Falta integrar el servicio para registrar al usuario
+      // JSON simulando usuarios
+      const userLogin = this.registerForm.value;
+      usersList.push(userLogin);
+      console.log('User Register -->', usersList);
+      this.router.navigate(['/ships']);
+    }
   }
 
 }
--- src/app/register/register.module.ts
+++ src/app/register/register.module.ts
@@ -2,6 +2,7 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {NgModule} from '@angular/core';
 import {RegisterComponent} from './components/register-component/register.component';
 import {RegisterRoutingModule} from './register-routing.module';
+import {SharedModule} from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -10,6 +11,7 @@ import {RegisterRoutingModule} from './register-routing.module';
   imports: [
     FormsModule,
     ReactiveFormsModule,
+    SharedModule,
     RegisterRoutingModule
   ],
 })
